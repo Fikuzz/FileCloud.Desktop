093087b404de499a69c1a5f18827f65e9afcb840
Franak Tumash <franak9@yandex.ru>
2025-08-17 19:12:46 +0300

    refactor: add error return in the service

--- FileCloud.Desctop/Services/FileService.cs
+++ FileCloud.Desctop/Services/FileService.cs
@@ -11,6 +11,7 @@ namespace FileCloud.Desktop.Services
     {
         private readonly HttpClient _client;
 
+        public bool ServerState { get; set; }
         public FileService(string apiBaseUrl)
         {
             _client = new HttpClient
@@ -18,23 +19,41 @@ namespace FileCloud.Desktop.Services
                 BaseAddress = new Uri(apiBaseUrl)
             };
         }
-
+        private (bool isActive, string error) IsServerActive()
+        {
+            string message = string.Empty;
+            if (!ServerState)
+                message = "Сервер закрыт.";
+            return (ServerState, message);
+        }
         /// <summary>
         /// Получить список всех файлов.
         /// </summary>
-        public async Task<List<FileModel>> GetFilesAsync()
+        public async Task<(List<FileModel>? result, string? error)> GetFilesAsync()
         {
-            return await _client.GetFromJsonAsync<List<FileModel>>("/api/file");
+            var conect = IsServerActive();
+            if (!conect.isActive)
+                return (null, conect.error);
+
+            return (await _client.GetFromJsonAsync<List<FileModel>>("/api/file"), string.Empty);
         }
-        public async Task<FileModel> GetFileByIdAsync(Guid id)
+        public async Task<(FileModel? result, string? error)> GetFileByIdAsync(Guid id)
         {
-            return await _client.GetFromJsonAsync<FileModel>($"/api/file/{id}");
+            var conect = IsServerActive();
+            if (!conect.isActive)
+                return (null, conect.error);
+
+            return (await _client.GetFromJsonAsync<FileModel>($"/api/file/{id}"), string.Empty);
         }
         /// <summary>
         /// Загрузить несколько файлов на сервер.
         /// </summary>
-        public async Task<string> UploadFilesAsync(string path, IEnumerable<string> filePaths)
+        public async Task<(string? result, string error)> UploadFilesAsync(string path, IEnumerable<string> filePaths)
         {
+            var conect = IsServerActive();
+            if (!conect.isActive)
+                return (null, conect.error);
+
             using var content = new MultipartFormDataContent();
 
             foreach (var filePath in filePaths)
@@ -47,38 +66,50 @@ namespace FileCloud.Desktop.Services
             var response = await _client.PostAsync($"/api/file/stream-upload?path=uploads", content);
             response.EnsureSuccessStatusCode();
 
-            return await response.Content.ReadAsStringAsync();
+            return (await response.Content.ReadAsStringAsync(), string.Empty);
         }
 
         /// <summary>
         /// Удалить файлы по списку идентификаторов.
         /// </summary>
-        public async Task<List<Guid>> DeleteFilesAsync(List<Guid> fileIds)
+        public async Task<(List<Guid>? result, string? error)> DeleteFilesAsync(List<Guid> fileIds)
         {
+            var conect = IsServerActive();
+            if (!conect.isActive)
+                return (null, conect.error);
+
             var response = await _client.PostAsJsonAsync("/api/file/delete", fileIds);
             response.EnsureSuccessStatusCode();
 
-            return await response.Content.ReadFromJsonAsync<List<Guid>>();
+            return (await response.Content.ReadFromJsonAsync<List<Guid>>(), string.Empty);
         }
 
         /// <summary>
         /// Получить превью изображения по идентификатору.
         /// </summary>
-        public async Task<ImageSource> GetPreviewAsync(Guid id)
+        public async Task<(ImageSource? result, string? error)> GetPreviewAsync(Guid id)
         {
+            var conect = IsServerActive();
+            if (!conect.isActive)
+                return (null, conect.error);
+
             var response = await _client.GetAsync($"/api/file/preview/{id}");
             response.EnsureSuccessStatusCode();
 
             var stream = await response.Content.ReadAsByteArrayAsync();
             var image = ImageHelper.ByteArrayToImageSource(stream);
 
-            return image;
+            return (image, string.Empty);
         }
         /// <summary>
         /// Получить файлы по идентификатору.
         /// </summary>
-        public async Task<string?> DownloadFileAsync(Guid id, string folder)
+        public async Task<(string? result, string? error)> DownloadFileAsync(Guid id, string folder)
         {
+            var conect = IsServerActive();
+            if (!conect.isActive)
+                return (null, conect.error);
+
             var response = await _client.GetAsync($"/api/file/download/{id}");
             if (response.IsSuccessStatusCode)
             {
@@ -93,12 +124,11 @@ namespace FileCloud.Desktop.Services
                 await File.WriteAllBytesAsync(savePath, fileBytes);
 
                 FileMappingManager.AddOrUpdate(id, savePath);
-                return newName;
+                return (newName, string.Empty);
             }
             else
             {
-                Console.WriteLine("error: Не удалось получить файл!");
-                return null;
+                return (null, "Не удалось получить файл!");
             }
         }
     }
