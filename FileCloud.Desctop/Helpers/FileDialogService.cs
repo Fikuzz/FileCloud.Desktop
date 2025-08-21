3feb407a2a2979f7058892aef96b7cc9b640c5a1
Franak Tumash <franak9@yandex.ru>
2025-08-21 19:18:23 +0300

    new FileDialogService class

---
+++ FileCloud.Desctop/Helpers/FileDialogService.cs
@@ -0,0 +1,23 @@
+﻿using FileCloud.Desktop.Services.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileCloud.Desktop.View.Helpers
+{
+    public class FileDialogService : IFileDialogService
+    {
+        public string[]? OpenFiles(string filter)
+        {
+            var dialog = new Microsoft.Win32.OpenFileDialog
+            {
+                Filter = filter,
+                Multiselect = true
+            };
+
+            return dialog.ShowDialog() == true ? dialog.FileNames : null;
+        }
+    }
+}
