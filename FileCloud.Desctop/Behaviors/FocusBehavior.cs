c50d1d717b5a2bb47805df7918c458163c96b239
Franak Tumash <franak9@yandex.ru>
2025-08-25 17:08:56 +0300

    add FocusBehavior

---
+++ FileCloud.Desctop/Behaviors/FocusBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace FileCloud.Desktop.View.Behaviors
+{
+    public class FocusBehavior
+    {
+        public static readonly DependencyProperty IsFocusedProperty =
+            DependencyProperty.RegisterAttached(
+                "IsFocused",
+                typeof(bool),
+                typeof(FocusBehavior),
+                new PropertyMetadata(false, OnIsFocusedChanged));
+
+        public static bool GetIsFocused(DependencyObject obj) =>
+            (bool)obj.GetValue(IsFocusedProperty);
+
+        public static void SetIsFocused(DependencyObject obj, bool value) =>
+            obj.SetValue(IsFocusedProperty, value);
+
+        private static void OnIsFocusedChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is TextBox tb && (bool)e.NewValue)
+            {
+                tb.Dispatcher.BeginInvoke(
+                   DispatcherPriority.Input,
+                   new Action(() =>
+                   {
+                       tb.Focus();
+                       tb.SelectAll();
+                       Keyboard.Focus(tb);
+                   }));
+            }
+        }
+    }
+}
