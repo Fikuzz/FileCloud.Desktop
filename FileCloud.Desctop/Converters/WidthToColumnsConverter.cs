5a9efbd9ce8391a4c8e842249dabcfe8206c2edc
Franak Tumash <franak9@yandex.ru>
2025-09-01 00:57:22 +0300

    feat: implement responsive grid layout for file items

---
+++ FileCloud.Desctop/Converters/WidthToColumnsConverter.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace FileCloud.Desktop.View.Converters
+{
+    public class WidthToColumnsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double containerWidth && parameter is string paramStr &&
+                double.TryParse(paramStr, NumberStyles.Any, culture, out double itemWidth))
+            {
+                if (itemWidth <= 0) return 1;
+
+                int columns = (int)Math.Floor(containerWidth / itemWidth);
+                return Math.Max(1, columns);
+            }
+            return 1;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
